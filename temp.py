d544e7a9c5223da279aef0b96543db88958c1ddb
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-11-15 18:29:00 +0200

    add skeleton files

---
+++ temp.py
@@ -0,0 +1,4 @@
+import sys
+print()
+print(sys.path)
+x = ['d:\\Semester 5\\Networks\\Project\\Code', 'C:\\Users\\Mohamed\\AppData\\Local\\Programs\\Python\\Python311\\python311.zip', 'C:\\Users\\Mohamed\\AppData\\Local\\Programs\\Python\\Python311\\DLLs', 'C:\\Users\\Mohamed\\AppData\\Local\\Programs\\Python\\Python311\\Lib', 'C:\\Users\\Mohamed\\AppData\\Local\\Programs\\Python\\Python311', 'C:\\Users\\Mohamed\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\site-packages']
